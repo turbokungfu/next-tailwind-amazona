3b02525b38856fce0d8afac644284ceb1d223db0
turbokungfu <slashrandazzo@gmail.com>
2022-12-07 15:38:36 -0800

    added balance to user model

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    balance: {type: Number, required: true, default: 0.00, min: 0.00 },
     isAdmin: { type: Boolean, required: true, default: false },
   },
   {
